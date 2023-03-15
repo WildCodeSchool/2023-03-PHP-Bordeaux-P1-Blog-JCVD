adc4952f376134a33ba83053206bbd8651ac2309
Aurélie Lozach <lozachaurelie@gmail.com>
2023-03-15 10:55:00 +0100

    création squelette projet JCVD

---
+++ _includes/head.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/CSS/main.css">
+    <title>Document</title>
+</head>
+<body>
+<p>test</p>
+<nav></nav>
+
+<header>
+
+</header>
+
+<article>
+    <card>
+        <div class="article1">
+        </div>
+        <div>
+            <h2>Article 1</h2>
+            <p>Bla bla bla</p>
+        </div>
+    </card>
+</article>
+</body>
+</html>
