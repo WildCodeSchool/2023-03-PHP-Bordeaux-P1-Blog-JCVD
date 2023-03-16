bb61af8d0fce544ee417a143b0d0015eeaea0c2e
Estephe Fath <estephefath@gmail.com>
2023-03-16 16:51:31 +0100

    1 tribute commit

---
+++ assets/poeme.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" href="/assets/CSS/main.css">
+<div class="tribute">
+    <div class="blockquotes">
+    <blockquote>
+        <p>Jean-Claude Van Damme, the man of might,</p>
+        <p>With muscles toned and shining bright,</p>
+        <p>He moves with grace, he strikes with power,</p>
+        <p>His kicks and punches shake the tower.</p>
+    </blockquote>
+
+    <blockquote>
+        <p>Born in Belgium, he started young,</p>
+        <p>Training hard until his skills were sprung,</p>
+        <p>From martial arts to film career,</p>
+        <p>He conquered hearts, he knew no fear.</p>
+    </blockquote>
+
+    <blockquote>
+        <p>In "Bloodsport" he was the hero,</p>
+        <p>In "Kickboxer" he fought the foe,</p>
+        <p>Universal Soldier" showed his might,</p>
+        <p>And "Timecop" took us to new heights.</p>
+    </blockquote>
+
+    <blockquote>
+        <p>His splits, his flips, his roundhouse kicks,</p>
+        <p>They all leave us in awe and fix,</p>
+        <p>Our eyes on him, our hearts ablaze,</p>
+        <p>As he dominates the screen and amaze.</p>
+    </blockquote>
+
+    <blockquote>
+        <p>With every role he takes on board,</p>
+        <p>He shows us what a star is formed,</p>
+        <p>For Jean-Claude Van Damme, we cheer,</p>
+        <p>Legend of action, always near.</p>
+        <cite> ChatGPT – March 2023</cite>
+    </blockquote>
+    </div>
+
+    <div class="tributeIMG">
+        <img src="/assets/images/poemeJCVD.jpeg" alt="image of JCVD">
+    </div>
+
+</div>
+
+<h3 id="titleRS">If you want more about JCVD click below!</h3>
+
+<div class="cercles">
+    <div id="facebook"> <a src="assets/images/JCVDRS.jpg" href="https://www.facebook.com/JCVDworld/?locale=fr_FR" </a></div>
+    <div id="instagram"><a src= "assets/images/logoInstagram.png" href="https://www.instagram.com/jcvd/?hl=fr"></a></div>
+    <div id="twitter"><a href="https://twitter.com/JCVD"></a></div>
+    <div id="shop"><a href="https://jcvdshop.com/"></a></div>
+</div>
+
+
+<!-- <a href="https://twitter.com/JCVD"><img src="/assets/images/JCVDRS.jpg"></a> -->
