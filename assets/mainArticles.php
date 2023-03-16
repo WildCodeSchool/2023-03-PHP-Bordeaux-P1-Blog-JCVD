594167f157093c2c18063d98fbb8e8e0588258f9
Aurélie Lozach <lozachaurelie@gmail.com>
2023-03-16 17:04:03 +0100

    first Article commit

---
+++ assets/mainArticles.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/modules/articles.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+<!--article "landscape" prenant 80% la largeur en mobile version et en version pc 80% de la largeur qui reste apres la side bar  -->
+<div class="containerMain">
+<div class="containerArticles">
+
+    <!-- article 3 roles marquants  -->
+    <card class="cardArticle">
+        <div class="containerImageArticle">
+            <a href="https://www.rtbf.be/article/retour-sur-trois-roles-marquants-dans-la-carriere-du-cultissime-jcvd-11164494" target="_blank">
+                <img src="../assets/images/3rolesMarquants.jpeg" alt="Triptyque des rôles marquants de JCVD" class="imageArticle">
+            </a>
+            <div class="appearCredit">
+                <p class="credit">Par Anaëlle Stévenart pour RTBF.be - Le 10 mars 2023</p>
+            </div>
+        </div>
+        <div class="descriptionArticle">
+            <h2>Retour sur trois rôles marquants dans la carrière du cultissime JCVD</h2>
+            <p>Né en 1960, Jean-Claude Van Damme a réussi à se créer une renommée internationale ! Dès l’âge de 10 ans, JCVD exerce le karaté et obtient même la ceinture noire à 18 ans.</p>
+        </div>
+    </card>
+
+    <!-- article Prédator  -->
+    <card class="cardArticle">
+        <div class="containerImageArticle">
+            <a href="https://www.ecranlarge.com/films/news/1070550-predator-jean-claude-van-damme-explique-les-vraies-raisons-de-son-depart-du-film" target="_blank">
+                <img src="../assets/images/predatorJCVD.webp" alt="Photo du Prédator" class="imageArticle">
+            </a>
+            <div class="appearCredit">
+                <p class="credit">La Rédaction @ecranlarge - Le 2 mars 2019</p>
+            </div>
+        </div>
+        <div class="descriptionArticle">
+            <h2>PREDATOR : JCVD explique les vraies raisons de son départ du film</h2>
+            <p>Le film d'action turbo-culte de John McTiernan avec Arnold Schwarzenegger devait également à l'origine inclure Jean-Claude Van Damme au casting. Enfin, presque...</p>
+        </div>
+    </card>
+
+    <!-- article Chuck Norris  -->
+    <card class="cardArticle smallCard">
+        <div class="containerImageArticle">
+            <a href="https://www.voici.fr/news-people/actu-people/il-mattaque-je-fais-un-coup-de-pied-retourne-jean-claude-van-damme-raconte-sa-rencontre-memorable-avec-chuck-norris-706917" target="_blank">
+                <img src="../assets/images/chucknorrisJCVD.jpeg" alt="Photo montage de Chuck Norris devant JCVD avant un combat" class="imageArticle">
+            </a>
+            <div class="appearCredit">
+                <p class="credit">Par Clémence Leroy pour Voici.fr - Le 26 juin 2021</p>
+            </div>
+        </div>
+        <div class="descriptionArticle">
+            <h2>Jean-Claude Van Damme raconte sa rencontre mémorable avec Chuck Norris</h2>
+            <p>Samedi 26 juin 2021, Jean-Claude Van Damme s'est confié dans 50' Inside sur ses débuts à Hollywood. Une carrière d'acteur qui a débuté avec la rencontre de Chuck Norris.</p>
+        </div>
+    </card>
+
+    <!-- article Racisme  -->
+    <card class="cardArticle smallCard">
+        <div class="containerImageArticle">
+            <a href="https://www.voici.fr/news-people/actu-people/jean-claude-van-damme-son-incroyable-technique-pour-contrer-le-racisme-dhollywood-envers-son-ami-708223" target="_blank">
+                <img src="../assets/images/racismeHollywwod.jpeg" alt="Photo montage de Tong Po maquillé en asiatique et de JVCD, en position de combat" class="imageArticle">
+            </a>
+            <div class="appearCredit">
+                <p class="credit">Par La Rédaction pour Voici.fr - Le 15 juillet 2021</p>
+            </div>
+        </div>
+        <div class="descriptionArticleArticle">
+            <h2>Son incroyable technique pour contrer le racisme d’Hollywood envers son ami</h2>
+            <p>Que serait Kickboxer sans son méchant, Tong Po ? Incarné par Mohamed Qissi, le producteur n'en voulait pas. Mais son ami JCVD a réussi à le faire recruter quand même.</p>
+        </div>
+    </card>
+
+    <!-- article Mortal Kombat  -->
+    <card class="cardArticle smallCard">
+        <div class="containerImageArticle">
+            <a href="https://www.cineserie.com/tv-vod/bloodsport-sur-amazon-prime-video-comment-le-film-a-inspire-le-jeu-video-mortal-kombat-3798456/" target="_blank">
+                <img src="../assets/images/bloodsport_mortal_kombat.jpeg" alt="Scène de combat dans le film Bloodsport" class="imageArticle">
+            </a>
+            <div class="appearCredit">
+                <p class="credit">Par Hatman pour CineSerie.com - le 19 octobre 2020</p>
+            </div>
+        </div>
+        <div class="descriptionArticle">
+            <h2>Comment Bloodsport a inspiré le jeu Mortal Kombat ?</h2>
+            <p>Le long-métrage a lancé la carrière de Jean-Claude Van Damme, et a surtout inspiré un certain nombre d’œuvres, y compris vidéoludiques. "Mortal Kombat" peut en témoigner.</p>
+        </div>
+    </card>
+
+    <!-- article AaRON Ultrarêve  -->
+    <card class="cardArticle smallCard">
+        <div class="containerImageArticle">
+            <a href="https://www.lefigaro.fr/musique/pour-ultrareve-son-nouveau-titre-aaron-fait-danser-jean-claude-van-damme-20200828" target="_blank">
+                <img src="../assets/images/UltrareveJCVD.png" alt="JCVD en train de danser dans le clip d'Aaron" class="imageArticle">
+            </a>
+            <div class="appearCredit">
+                <p class="credit">Par Théo Rampazzo pour lefigaro.fr - Le 28 août 2020</p>
+            </div>
+        </div>
+        <div class="descriptionArticle">
+            <h2>Pour Ultrarêve, son nouveau titre, AaRON fait danser Jean-Claude Van Damme</h2>
+            <p>Le duo français s'est offert la présence de l'acteur, qui a tourné le clip avec ses enfants à Los Angeles pendant le confinement.</p>
+        </div>
+    </card>
+
+</div>
+
+<aside class="asideRelative">
+    <h2>TOP 3</h2>
+    <a href="https://fr.news.yahoo.com/s%C3%A9cheresse-interview-jean-claude-van-135458344.html" class="top3" target="_blank">
+        <strong>Avec la sècheresse, cette mémorable interview refait surface</strong>
+    </a>
+    <a href="https://fr.news.yahoo.com/s%C3%A9cheresse-interview-jean-claude-van-135458344.html" class="top3" target="_blank">
+        <strong>Article 2 à modifier le lien et le titre</strong>
+    </a>
+    <a href="https://fr.news.yahoo.com/s%C3%A9cheresse-interview-jean-claude-van-135458344.html" class="top3" target="_blank">
+        <strong>Article 3 à modifier le lien et le titre</strong>
+    </a>
+</aside>
+
+</div>
+
+
+</body>
+</html>
