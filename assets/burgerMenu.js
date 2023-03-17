1dacbe17b5f2302812911d92989e6bd1f26832c3
TOMAS SPIT <Tomas.p.spit1@gmail.com>
2023-03-17 16:32:01 +0100

    Tried adding the "sticky navbar" WIP

--- assets/burgerMenu.js
+++ assets/burgerMenu.js
@@ -3,3 +3,19 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+window.onscroll = function() {myFunction()};
+
+/* Sticky nav bar */
+
+let navbar = document.getElementsByClassName("nameBar");
+
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
