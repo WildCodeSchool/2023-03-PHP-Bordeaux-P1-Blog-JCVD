9bc440dea95f926b7f6b3998367d7e3882348990
TOMAS SPIT <Tomas.p.spit1@gmail.com>
2023-03-17 16:38:22 +0100

    Divised the 2 navbars into 2 different PHP files for easier use

--- _includes/headOtherPages.php
+++ _includes/headOtherPages.php
@@ -1,1 +1,33 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/CSS/modules/headOtherPages.css">
+    <script src="/assets/burgerMenu.js"></script>
+    <title>Document</title>
+</head>
+
+<div class="nameBar">
+    <div id="menu">
+        <div id="menu-bar" onclick="menuOnClick()">
+            <div id="bar1" class="bar"></div>
+            <div id="bar2" class="bar"></div>
+            <div id="bar3" class="bar"></div>
+        </div>
+        <nav class="nav" id="nav">
+            <ul>
+                <li><a href="/assets/mainArticles.php">Home</a></li>
+                <li><a href="/assets/poeme.php">Tribute</a></li>
+                <li><a href="/assets/movies.php">Filmographie</a></li>
+                <li><a href="/assets/quotes.php">Famous Quotes</a></li>
+                <li><a href="/assets/aboutUs.php">About us</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="menu-bg" id="menu-bg"></div>
+    <h1 class="textTitle">JEAN CLAUDE VAN DAMME</h1>
+</div>
