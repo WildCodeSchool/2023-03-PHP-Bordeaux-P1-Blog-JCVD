1dcb02e2286a22a0e949397e420d2e26b5c252fd
TOMAS SPIT <Tomas.p.spit1@gmail.com>
2023-03-21 14:52:46 +0100

    Added the disclaimer

---
+++ assets/disclaimer.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/CSS/modules/headOtherPages.css">
+    <link rel="stylesheet" href="/assets/CSS/main.css">
+    <link rel="stylesheet" href="/assets/CSS/modules/footer.css">
+    <script src="/assets/burgerMenu.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+<div class="nameBar">
+    <div id="menu">
+        <div id="menu-bar" onclick="menuOnClick()">
+            <div id="bar1" class="bar"></div>
+            <div id="bar2" class="bar"></div>
+            <div id="bar3" class="bar"></div>
+        </div>
+        <nav class="nav" id="nav">
+            <ul>
+                <li><a href="/assets/mainArticles.php">Home</a></li>
+                <li><a href="/assets/poeme.php">Tribute</a></li>
+                <li><a href="/assets/movies.php">Filmographie</a></li>
+                <li><a href="/assets/quotes.php">Famous Quotes</a></li>
+                <li><a href="/assets/aboutUs.php">About us</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="menu-bg" id="menu-bg"></div>
+    <h1 class="textTitle">JEAN CLAUDE VAN DAMME - UNOFFICIAL BLOG</h1>
+</div>
+
+<div class="Disclaimer">
+
+    <h1>
+        General Disclaimer
+    </h1>
+
+    <br><br>
+
+<p>
+The information contained in this website is for general information purposes only. The information is provided by our unofficial blog and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+
+In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+
+Through this website you are able to link to other websites which are not under the control of our unofficial blog. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+
+Every effort is made to keep the website up and running smoothly. However, our unofficial blog takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control. </p>
+
+
+</div>
+
+
+<footer>
+    <div class="footer">
+        <ul class="ul-footer">
+            <li><a href="/assets/disclaimer.php">Disclaimer</a></li>
+            <li><a href="/assets/aboutUs.php">About us</a></li>
+        </ul>
+    </div>
+</footer>
+
+</body>
+</html>
